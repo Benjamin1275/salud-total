fbda0c8a0f2fa5b6fa36960a7a8a653373b5db1a
KANBSETR <azobisiso576@gmail.com>
2025-05-01 19:39:44 -0400

    Horas Atención

--- frontend/src/app/pages/reserva/datos-paciente/datos-paciente.component.ts
+++ frontend/src/app/pages/reserva/datos-paciente/datos-paciente.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-
+import { CardComponent } from '../../../components/card/card.component';
+import { FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-datos-paciente',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './datos-paciente.component.html',
   styleUrl: './datos-paciente.component.css'
 })
 export class DatosPacienteComponent {
+  form: FormGroup = new FormGroup({});
+  constructor() { }
 
+  ngOnInit(): void {
+    this.form = new FormGroup({});
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
--- frontend/src/app/pages/reserva/horas-atencion/horas-atencion.component.ts
+++ frontend/src/app/pages/reserva/horas-atencion/horas-atencion.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, model } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { CardComponent } from '../../../components/card/card.component';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
 
 @Component({
   selector: 'app-horas-atencion',
-  imports: [],
+  imports: [MatCardModule, MatDatepickerModule, CardComponent, MatListModule, MatButtonModule, MatFormFieldModule, MatSelectModule, MatInputModule ],
   templateUrl: './horas-atencion.component.html',
-  styleUrl: './horas-atencion.component.css'
+  providers: [provideNativeDateAdapter()],
+  styleUrl: './horas-atencion.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HorasAtencionComponent {
+  selected = model<Date | null>(null);
 
+  //ejemplo de un array de horas
+  horas = [
+    { id: 1, hora: '08:00', disponible: true },
+    { id: 2, hora: '09:00', disponible: true },
+    { id: 3, hora: '10:00', disponible: false },
+    { id: 4, hora: '11:00', disponible: true },
+    { id: 5, hora: '12:00', disponible: false },
+    { id: 6, hora: '13:00', disponible: true },
+    { id: 7, hora: '14:00', disponible: true },
+    { id: 8, hora: '15:00', disponible: false },
+    { id: 9, hora: '16:00', disponible: true },
+    { id: 10, hora: '17:00', disponible: true },
+  ];
+  medicos: Food[] = [
+    { value: 'steak-0', viewValue: 'Benjamín' },
+    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'tacos-2', viewValue: 'Tacos' },
+  ];
 }
