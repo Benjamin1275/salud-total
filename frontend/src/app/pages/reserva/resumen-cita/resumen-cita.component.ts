dfc7f9c77190eef1d6e18c0f1eb06cb7e306219c
KANBSETR <azobisiso576@gmail.com>
2025-05-01 20:02:55 -0400

    Resumen cita

--- frontend/src/app/pages/reserva/resumen-cita/resumen-cita.component.ts
+++ frontend/src/app/pages/reserva/resumen-cita/resumen-cita.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../../components/card/card.component';
 
+
+type Cita ={
+  id: number;
+  fecha: string;
+  hora: string;
+  nombre: string;
+}
 @Component({
   selector: 'app-resumen-cita',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './resumen-cita.component.html',
   styleUrl: './resumen-cita.component.css'
 })
+
 export class ResumenCitaComponent {
 
+  cita: Cita = {
+    id: 1,
+    fecha: '2023-10-01',
+    hora: '10:00 AM',
+    nombre: 'Juan Pérez',
+  }
+
+  confirmarCita() {
+    alert('Cita confirmada!');
+  }
 }
